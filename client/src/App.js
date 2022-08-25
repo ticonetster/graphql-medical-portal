f3e26251fb27cc92b7b2a6d311f2ef2a62ac8426
Elliott <elliottli.job@gmail.com>
2022-08-24 20:54:38 -0700

    added react router into App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,22 @@
 import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <div className="health-app">
+      <Router>
+        <div className="container">
+          <Routes>
+            <Route
+              path="/"
+              element={<placeholder/>}
+            />
+            <Route
+              path="/test/"
+              element={<placeholder/>}
+            />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
