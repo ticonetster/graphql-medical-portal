df05b1eeaaf5af9d47147df6db80dbf2a5a4a425
Elliott <elliottli.job@gmail.com>
2022-09-11 14:36:25 -0700

    fixed seed data , changed patient model

--- server/seed/seed.js
+++ server/seed/seed.js
@@ -14,7 +14,7 @@ connection.once('open', async () => {
     await Appointments.deleteMany({});
     //await Schedules.deleteMany({});
     const historyData = [{
-        "_id":"631e42f136cc835430e2a250",
+        "_id": "631e42f136cc835430e2a250",
         "height": "5'8''",
         "weight": "180",
         "allergies": "latex",
@@ -22,7 +22,7 @@ connection.once('open', async () => {
         "data": "Previous history: Had a cold",
     },
     {
-        "_id":"631e42f136cc835430e2a24f",
+        "_id": "631e42f136cc835430e2a24f",
         "height": "5'3''",
         "weight": "130",
         "allergies": "latex",
@@ -31,23 +31,25 @@ connection.once('open', async () => {
     }]
     const doctorData = [
         {
-            "_id":"631e428eba8096686455036f",
+            "_id": "631e428eba8096686455036f",
             "username": "jakesmith",
             "name": "Jake Smith",
             "gender": "male",
             "email": "jakesmith@hospital.com",
-            "password": "j@kesm1th"
+            "password": "j@kesm1th",
+            "appointments":["631e42f136cc835430e2a24f"],
         },
         {
-            "_id":"631e428eba8096686455036e",
+            "_id": "631e428eba8096686455036e",
             "username": "robbrown",
             "name": "Rob Brown",
             "gender": "male",
             "email": "robbrown@hospital.com",
-            "password": "r0bbr0wn"
+            "password": "r0bbr0wn",
+            "appointments":["631e42f136cc835430e2a250"],
         },
         {
-            "_id":"631e428eba80966864550370",
+            "_id": "631e428eba80966864550370",
             "username": "susangartner",
             "name": "Susan Gartner",
             "gender": "female",
@@ -62,43 +64,50 @@ connection.once('open', async () => {
     //console.log(doctorData[0])
     patientData.push(
         {
-            "_id":"631e424c632b1c3c14bd15f2",
+            "_id": "631e424c632b1c3c14bd15f2",
             "username": "Test",
             "name": "Test",
             "gender": "Test",
             "email": "test@test.com",
             "password": "testtest",
             "address": "123 Main St",
-            "primaycareteam": [doctorData[0]._id],
+            "appointments":["631e42f136cc835430e2a24f"],
+            "primarycareteam": [doctorData[0]._id],
             "history": historyData[0]._id
         },
         {
-            "_id":"631e428eba80966864550375",
+            "_id": "631e428eba80966864550375",
             "username": "princessdisco",
             "name": "Princess Disco",
             "gender": "female",
             "email": "princessdisco@gmail.com",
             "password": "d1sc0b@ll",
             "address": "123 Main St",
-            "primaycareteam": [doctorData[1]._id],
+            "appointments":["631e42f136cc835430e2a250"],
+            "primarycareteam": [doctorData[1]._id],
             "history": historyData[1]._id
         }
     )
-    await Patients.create(patientData)
-    AppointmentData.push({
-        "status": "Not Started",
-        "dateTime": "2022-10-15T09:00",
-        "concern": "I have a migraine",
-        "patient": patientData[1]._id,
-        "doctor": doctorData[1]._id
-    },
+
+    await Patients.create(patientData) //hashes password doesnt insert array keys
+    AppointmentData.push(
         {
+            "_id": "631e42f136cc835430e2a250",
+            "status": "Not Started",
+            "dateTime": "2022-10-15T09:00",
+            "concern": "I have a migraine",
+            "patient": patientData[1]._id,
+            "doctor": doctorData[1]._id
+        },
+        {
+            "_id": "631e42f136cc835430e2a24f",
             "status": "Test",
             "dateTime": "2022-10-15T09:00",
             "concern": "Test",
             "patient": patientData[0]._id,
             "doctor": doctorData[0]._id
         })
+    //await Appointments.collection.insertMany(AppointmentData)
     await Appointments.create(AppointmentData)
     console.log(patientData)
 
@@ -111,3 +120,4 @@ connection.once('open', async () => {
     console.info('Seeding complete! 🌱');
     process.exit(0);
 });
+
